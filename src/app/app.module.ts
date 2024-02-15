4412ff8b9eef871de87b570bc8350924356a4e2d
Okayami <46897189+Okayami@users.noreply.github.com>
2024-02-15 19:58:35 +0100

    Add basic routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+
+export const appRouteList: Routes = [
+  {
+      path: 'landing',
+      component: FirstComponent
+  },
+  {
+      path: 'search',
+      component: SecondComponent
+  },
+  {
+      path: '**',
+      redirectTo: 'landing'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  exports: [
+    RouterModule
+  ],
   imports: [
+    RouterModule.forRoot(appRouteList),
     BrowserModule
   ],
   providers: [],
